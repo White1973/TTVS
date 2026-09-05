d5e3cf2f0266e58d2cb40a895eed0920b962d344
Bai Kai <sbaiae@connect.ust.hk>
2026-09-05 08:48:56 -0700

    Update preprocess_mix.py

--- data/preprocess_mix.py
+++ data/preprocess_mix.py
@@ -41,7 +41,7 @@ def make_map_fn(split, source=None):
 
 if __name__ == '__main__':
 
-    data_source = 'MATH-TTT'
+    data_source = 'AIME-TTT'
 
     train_dataset = datasets.load_dataset("json", data_files=os.path.join(data_source, 'train_mix_new.json'), split='train')
     #test_dataset = datasets.load_dataset("json", data_files=os.path.join(data_source, 'test.json'), split='train')
